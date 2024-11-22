991df3b89ff3eaaf7ca24d39338a3364665fcc5d
HallZero <filipikikuchi@gmail.com>
2024-11-22 10:39:57 -0300

    feat(semana1): :sparkles: Add semana 1 solution

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # VR-optional-individual
+
+
+## Semana 1
+Aplicativo que permite o usuário escolher entre 2 experiências envolvendo realidade virtual ou realidade aumentada.
---
+++ src/semana1/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+// Enable image loading from the current directory
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    // Render the a.html file
+    res.sendFile(__dirname + '/choose.html');
+    }
+);
+
+app.get('/vr', (req, res) => {
+    // Render the a.html file
+    res.sendFile(__dirname + '/vr.html');
+    }
+);
+
+app.get('/ar', (req, res) => {
+    // Render the a.html file
+    res.sendFile(__dirname + '/ar.html');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
